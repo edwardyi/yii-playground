06ad9e32530bb20e2c39e5ae4df69b7eb7696906
adgeekedwardai <edward.ai@adgeek.com.tw>
2021-06-18 14:35:48 +0000

    [init] docker-compose file and testing db, redis connection

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic', // db 172.22.0.4
+    'username' => 'user',
+    'password' => '1234',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
